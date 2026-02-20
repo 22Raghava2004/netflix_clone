fa223f2c8fe4e347607cdd13ce63afff6ccba6bf
22Raghava2004 <raghavangowda2004@gmail.com>
2026-02-20 13:05:36 +0530

    11

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 def index(request):
     return render(request, "index.html")
 
@@ -10,7 +11,32 @@ def login(request):
     return render(request,'login.html')
 
 def signup(request):
-    return render(request,'signup.html')
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+
+        if password==password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'username already exists')
+                return redirect('signup')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                
+                #user login
+                user_login=auth.authenticate(username=username,password=password)
+                auth.login(request,user_login)
+                return redirect('/')
+        else:
+            messages.info(request,'password not matching')
+            return redirect('signup')
+    else:
+        return render(request,'signup.html')
 
 
 
--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'netflix_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ #DATABASES = {
+  #  'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'postgres',
+        'USER':'postgres',
+        'PASSWORD':'#Raghavan777@1234',
+        'HOST':'db.fativvzenaztfjlmshev.supabase.co',
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
