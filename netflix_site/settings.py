6f3de719585f9098979e8ba11e4575c5a1e3ce75
22Raghava2004 <raghavangowda2004@gmail.com>
2026-03-26 13:01:31 +0530

    fix static files

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -143,3 +143,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
