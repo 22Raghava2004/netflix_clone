96c41bd2ffb17a27ea16dedabce574bae6d9a550
22Raghava2004 <raghavangowda2004@gmail.com>
2026-03-26 17:10:42 +0530

    debud fix

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fh49&lib29v2g*lru0=j*6(l34_3$cv+8=@2%@2znj))8!uyej'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
